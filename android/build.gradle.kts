9a4c3995066100c1930c1be9cbee0ece7e0064ce
Faralha <faralhaonly@gmail.com>
2026-06-10 01:36:21 +0700

    fix: gradle build

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,25 +1,16 @@
-pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-        flutterSdkPath
-    }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
+allprojects {
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 
-plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.2.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+rootProject.buildDir = file("../build")
+subprojects {
+    project.buildDir = file("${rootProject.buildDir}/${project.name}")
+    project.evaluationDependsOn(":app")
 }
 
-include(":app")
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
