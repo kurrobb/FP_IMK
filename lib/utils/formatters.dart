0e25a82a843b8975cffd91874d95b3bf7c5c1f6a
Raditya Yusuf <136904992+eggseedd@users.noreply.github.com>
2026-06-22 12:16:51 +0700

    sisa2

--- lib/utils/formatters.dart
+++ lib/utils/formatters.dart
@@ -46,3 +46,41 @@ class AccountNumberFormatter extends TextInputFormatter {
     );
   }
 }
+
+/// Formats phone number input into groups of 4 digits separated by spaces (3x4 format)
+/// Example: "081578979853" -> "0815 7897 9853"
+class PhoneNumberFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    // Only allow digits
+    final digitsOnly = newValue.text.replaceAll(RegExp(r'\D'), '');
+
+    // Limit to 12 digits (3 groups of 4)
+    final maxLength = 12;
+    final limitedDigits = digitsOnly.length > maxLength
+        ? digitsOnly.substring(0, maxLength)
+        : digitsOnly;
+
+    // Format into 3 groups of 4 digits
+    final buffer = StringBuffer();
+    for (int i = 0; i < limitedDigits.length; i++) {
+      if (i > 0 && i % 4 == 0) {
+        buffer.write(' ');
+      }
+      buffer.write(limitedDigits[i]);
+    }
+
+    final formattedText = buffer.toString();
+    
+    // Calculate cursor position
+    int cursorOffset = formattedText.length;
+
+    return TextEditingValue(
+      text: formattedText,
+      selection: TextSelection.collapsed(offset: cursorOffset),
+    );
+  }
+}
